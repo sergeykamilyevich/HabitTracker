14fe97892af6bf5def9190bd8145127a68d0f073
Sergey <sergey.kamilyevich@gmail.com>
2022-04-01 19:15:37 +0700

    cleaned up code

--- app/src/main/java/com/example/habittracker/presentation/view_models/Event.kt
+++ app/src/main/java/com/example/habittracker/presentation/view_models/Event.kt
@@ -3,7 +3,6 @@ package com.example.habittracker.presentation.view_models
 open class Event<out T>(private val data: T) {
 
     private var hasBeenHandled = false
-//        private set
 
     fun transferIfNotHandled(): T? = if (hasBeenHandled) null
     else {
