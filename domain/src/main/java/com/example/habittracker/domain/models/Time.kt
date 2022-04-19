37262b745cdc09bde50d3b1badbb67a33d2a476b
Sergey <sergey.kamilyevich@gmail.com>
2022-04-19 22:55:52 +0700

    fixed problem with timezone

--- domain/src/main/java/com/example/habittracker/domain/models/Time.kt
+++ domain/src/main/java/com/example/habittracker/domain/models/Time.kt
@@ -5,27 +5,26 @@ import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
+
 @Singleton
-class HabitTime @Inject constructor() {
+class Time @Inject constructor() {
 
     fun getCurrentUtcDateInInt(): Int {
         val currentMoment = Calendar.getInstance()
         val timeInMillis = currentMoment.timeInMillis
-        val dateWithoutOffset = timeInMillis / MILLIS_IN_DAY
+        val dateWithoutOffset = timeInMillis / MILLIS_IN_SECOND
         return dateWithoutOffset.toInt()
     }
 
-    fun mapUtcDateInIntToString(date: Int): String {
-        val currentMoment = Calendar.getInstance()
-        val offsetInMillis = currentMoment.timeZone.rawOffset
-        val timeInMillis = date.toLong() * MILLIS_IN_DAY + offsetInMillis
+    fun mapUtcDateInIntToString(dateInSecond: Int): String {
+        val timeInMillis = dateInSecond.toLong() * MILLIS_IN_SECOND
         val locale = Locale.getDefault()
         val sdf = SimpleDateFormat("dd-MM-yyyy", locale)
         return sdf.format(timeInMillis)
     }
 
     companion object {
-        const val MILLIS_IN_DAY = 24 * 60 * 60 * 1000
+        const val MILLIS_IN_SECOND = 1000
 
     }
 }
