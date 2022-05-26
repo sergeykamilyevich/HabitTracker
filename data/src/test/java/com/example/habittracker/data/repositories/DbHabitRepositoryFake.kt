f4ba90d5fba3d3464f36392f19dc86f8d82b5a3e
Sergey <sergey.kamilyevich@gmail.com>
2022-05-26 17:30:04 +0700

    added tests for DbHabitRepositoryImpl

--- data/src/test/java/com/example/habittracker/data/repositories/DbHabitRepositoryFake.kt
+++ data/src/test/java/com/example/habittracker/data/repositories/DbHabitRepositoryFake.kt
@@ -5,6 +5,7 @@ import com.example.habittracker.domain.errors.IoError
 import com.example.habittracker.domain.errors.failure
 import com.example.habittracker.domain.errors.success
 import com.example.habittracker.domain.models.*
+import com.example.habittracker.domain.models.HabitListOrderBy.*
 import com.example.habittracker.domain.repositories.DbHabitRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -12,8 +13,8 @@ import kotlinx.coroutines.runBlocking
 
 class DbHabitRepositoryFake : DbHabitRepository {
 
-    private val habits = mutableListOf<Habit>()
-    private val habitDones = mutableListOf<HabitDone>()
+    private val habitList = mutableListOf<Habit>()
+    private val habitDoneList = mutableListOf<HabitDone>()
     private var indexHabits = 0
     private var indexHabitDones = 0
 
@@ -37,9 +38,9 @@ class DbHabitRepositoryFake : DbHabitRepository {
 
 
     fun importHabits(habitList: List<Habit>) {
-        habits.clear()
+        this.habitList.clear()
         habitList.forEach {
-            habits.add(it)
+            this.habitList.add(it)
         }
     }
 
@@ -69,17 +70,17 @@ class DbHabitRepositoryFake : DbHabitRepository {
         habitListFilter: HabitListFilter
     ): Flow<List<Habit>> {
         val filteredByTypeList =
-            if (habitType != null) habits.filter { it.type == habitType } else habits
+            if (habitType != null) habitList.filter { it.type == habitType } else habitList
         val filteredBySearchList = filteredByTypeList.filter {
             it.name.contains(habitListFilter.search)
         }
         val filteredList = when (habitListFilter.orderBy) {
-            HabitListOrderBy.NAME_ASC -> filteredBySearchList.sortedBy { it.name }
-            HabitListOrderBy.NAME_DESC -> filteredBySearchList.sortedByDescending { it.name }
-            HabitListOrderBy.PRIORITY_ASC -> filteredBySearchList.sortedBy { it.priority.id }
-            HabitListOrderBy.PRIORITY_DESC -> filteredBySearchList.sortedByDescending { it.priority.id }
-            HabitListOrderBy.TIME_CREATION_ASC -> filteredBySearchList.sortedBy { it.date }
-            HabitListOrderBy.TIME_CREATION_DESC -> filteredBySearchList.sortedByDescending { it.date }
+            NAME_ASC -> filteredBySearchList.sortedBy { it.name }
+            NAME_DESC -> filteredBySearchList.sortedByDescending { it.name }
+            PRIORITY_ASC -> filteredBySearchList.sortedBy { it.priority.id }
+            PRIORITY_DESC -> filteredBySearchList.sortedByDescending { it.priority.id }
+            TIME_CREATION_ASC -> filteredBySearchList.sortedBy { it.date }
+            TIME_CREATION_DESC -> filteredBySearchList.sortedByDescending { it.date }
         }
         return flow {
             emit(filteredList)
@@ -87,28 +88,28 @@ class DbHabitRepositoryFake : DbHabitRepository {
     }
 
     override suspend fun getUnfilteredList(): Either<IoError, List<Habit>> =
-        if (errorReturn) IoError.SqlError().failure() else habits.success()
+        if (errorReturn) IoError.SqlError().failure() else habitList.success()
 
     override suspend fun getHabitById(habitId: Int): Either<IoError, Habit> =
-        (habits.find { it.id == habitId })?.success()
+        (habitList.find { it.id == habitId })?.success()
             ?: IoError.SqlError("Habit with id $habitId not found").failure()
 
     override suspend fun upsertHabit(habit: Habit): Either<IoError, Int> {
         if (errorReturn) return IoError.SqlError().failure()
         if (habit.id == 0) {
             indexHabits++
-            habits.add(habit.copy(id = indexHabits))
+            habitList.add(habit.copy(id = indexHabits))
             return indexHabits.success()
         }
-        return habits.find { it.id == habit.id }?.let {
-            habits.remove(it)
-            habits.add(habit)
+        return habitList.find { it.id == habit.id }?.let {
+            habitList.remove(it)
+            habitList.add(habit)
             ITEM_NOT_ADDED.success()
         } ?: IoError.SqlError("Habit with id ${habit.id} not found").failure()
     }
 
     override suspend fun deleteHabit(habit: Habit): Either<IoError, Unit> {
-        val resultOfDeleting = habits.remove(habit)
+        val resultOfDeleting = habitList.remove(habit)
         return if (resultOfDeleting) Unit.success() else IoError.SqlError("Habit $habit not found")
             .failure()
     }
@@ -116,7 +117,7 @@ class DbHabitRepositoryFake : DbHabitRepository {
     override suspend fun deleteAllHabits(): Either<IoError, Unit> =
         if (errorReturn) IoError.SqlError("Error while deleting habit").failure()
         else {
-            habits.clear()
+            habitList.clear()
             Unit.success()
         }
 
@@ -124,25 +125,25 @@ class DbHabitRepositoryFake : DbHabitRepository {
         if (errorReturn) return IoError.SqlError().failure()
         if (habitDone.id == 0) {
             indexHabitDones++
-            habitDones.add(habitDone.copy(id = indexHabitDones))
+            habitDoneList.add(habitDone.copy(id = indexHabitDones))
             return indexHabitDones.success()
         }
-        return habitDones.find { it.id == habitDone.id }?.let {
-            habitDones.remove(it)
-            habitDones.add(habitDone)
+        return habitDoneList.find { it.id == habitDone.id }?.let {
+            habitDoneList.remove(it)
+            habitDoneList.add(habitDone)
             ITEM_NOT_ADDED.success()
         } ?: IoError.SqlError("HabitDone with id ${habitDone.id} not found").failure()
     }
 
     override suspend fun deleteHabitDone(habitDoneId: Int) {
-        habitDones.find { it.id == habitDoneId }?.let {
-            habitDones.remove(it)
+        habitDoneList.find { it.id == habitDoneId }?.let {
+            habitDoneList.remove(it)
         }
     }
 
-    fun findHabit(habit: Habit) = habits.find { it == habit }
+    fun findHabit(habit: Habit) = habitList.find { it == habit }
 
-    fun findHabitDone(habitDone: HabitDone) = habitDones.find {
+    fun findHabitDone(habitDone: HabitDone) = habitDoneList.find {
         it.date == habitDone.date && it.habitUid == habitDone.habitUid
     }
 
