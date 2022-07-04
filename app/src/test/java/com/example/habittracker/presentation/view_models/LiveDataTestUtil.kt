771dae82bbd515dd486a000472c691dd9ae58152
Sergey <sergey.kamilyevich@gmail.com>
2022-07-04 17:26:13 +0700

    add tests for MainViewModel

---
+++ app/src/test/java/com/example/habittracker/presentation/view_models/LiveDataTestUtil.kt
@@ -0,0 +1,38 @@
+import androidx.annotation.VisibleForTesting
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+private const val WAIT_SECONDS_BEFORE_THROW = 1L
+
+@VisibleForTesting(otherwise = VisibleForTesting.NONE)
+fun <T> LiveData<T>.getOrAwaitValue(
+    time: Long = WAIT_SECONDS_BEFORE_THROW,
+    timeUnit: TimeUnit = TimeUnit.SECONDS,
+    afterObserve: () -> Unit = {}
+): T {
+    var data: T? = null
+    val latch = CountDownLatch(1)
+    val observer = object : Observer<T> {
+        override fun onChanged(newData: T?) {
+            data = newData
+            latch.countDown()
+            this@getOrAwaitValue.removeObserver(this)
+        }
+    }
+    this.observeForever(observer)
+
+    try {
+        afterObserve.invoke()
+        if (!latch.await(time, timeUnit)) {
+            throw TimeoutException("LiveData value was never set")
+        }
+    } finally {
+        this.removeObserver(observer)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    return data as T
+}
